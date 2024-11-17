bd2382b525161d0569995a96a676c12f0ff453bd
ScarletExplorer <zlyakhov240204@gmail.com>
2024-11-17 14:54:13 +0300

    task_1.py

--- Lab_4/task_1.py
+++ Lab_4/task_1.py
@@ -1,10 +1,10 @@
 # TODO решите задачу
 import json
 
-input_filename = "input.json"
+file_ = "input.json"
 
 def task() -> float:
-    with open(input_filename, 'r', encoding='utf-8') as file:
+    with open(file_, 'r', encoding='utf-8') as file:
         data = json.load(file)
         total = sum(item['score'] * item['weight'] for item in data)
         return round(total, 3)
